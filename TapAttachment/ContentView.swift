97e9798c637cedb06468a180b4df10763f2316f9
Tim Mitra <tim@it-guy.com>
2024-09-26 02:10:28 -0400

    Initial Commit

---
+++ TapAttachment/ContentView.swift
@@ -0,0 +1,30 @@
+//
+// ---------------------------- //
+// Original Project: TapAttachment
+// Created on 2024-09-26 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ TapAttachment/TapAttachmentApp.swift
@@ -0,0 +1,21 @@
+//
+// ---------------------------- //
+// Original Project: TapAttachment
+// Created on 2024-09-26 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct TapAttachmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
